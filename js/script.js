d736ebbd43df97c4ad92e41849d37f5713daf199
Mays88 <antmays.ceo@gmail.com>
2024-04-02 15:42:07 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// INTRODUCTION
+
+const fullName = "Anthony Mays";
+let currentCity = "Dearborn Heights";
+console.log(`Hello, my name is ${fullName} and I live in ${currentCity}`);
+
+// AGE BREAKDOWN
+
+const age = 35;
+console.log(`My current age is ${age}`);
+
+console.log("Today has been a good day");
+
+const inTenYears = age + 10;
+console.log(`In 10 years I will be ${inTenYears} years young.`);
+
+const ofAge = age > 21;
+
+console.log(`It is ${ofAge} that I am of drinking age.`);
+
+// GREETING
+
+const string1 = "Welcome, ";
+const string2 = "Nice to meet you!";
+
+console.log(string1 + string2);
+
+// QUOTE OF THE DAY
+
+let quote =
+    "Progress means getting nearer to the place you want to be.\nAnd if you have taken a wrong turn, then to go forward does not get you any nearer.\nIf you are on the wrong road, progress means doing an about-turn and walking back to the right road;\nand in that case, the man who turns back soonest is the most progressive man. \n -C.S Lewis";
+
+console.log(quote);
